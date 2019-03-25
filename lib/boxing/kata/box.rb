7cfeeac15ebeaa620ebb5af8a2ad122bcc3da75d
shaddox93 <haddoxshannon93@gmail.com>
2019-03-24 21:08:20 -0400

    add box methods and comments

--- lib/boxing/kata/box.rb
+++ lib/boxing/kata/box.rb
@@ -1,10 +1,25 @@
 module Boxing
   module Kata
     class Box
+
+      #----------------------------------------------------#
+      # Box Class: a new box object for each box that needs
+      # created for the family.
+      #
+      # Has instance variables: brushCount, boxContents,
+      # boxSize, and weight 
+      #----------------------------------------------------#
+
+      attr_reader(
+        :boxContents,
+        :brushCount,
+        :weight
+      )
+
       def initialize(boxSize)
         @brushCount = 0
-        @boxContents = Hash.new
-        @boxSize = boxSize
+        @boxContents = Hash.new #used to keep track of each item in the box
+        @boxSize = boxSize # determines if starter box or refill box
         @weight = 0
 
         if @boxSize == 2
@@ -15,7 +30,13 @@ module Boxing
       end
 
       def add(brushColor)
-
+        # adds a brush color to the box hash
+        if @boxContents.has_key?(brushColor)
+          @boxContents[brushColor] += 1
+        else
+          @boxContents[brushColor] = 1
+        end
+        @brushCount +=1
       end
 
       def full?
@@ -27,19 +48,64 @@ module Boxing
       end
 
       def calcShipping
-
+        # 16oz is the min weight a box can have to be priority
+        if @weight > 16
+          puts "Shipping: Priority"
+          #Priority Shipping
+        else
+          #First Class Shipping
+          puts "Shipping: First"
+        end
       end
 
-      def calcWeight(value)
 
+      def calcWeight(value)
+        # weight is determined as:
+        # brush - 9oz
+        # replacement head - 1oz
+        # paste kit - 7.6oz
+
+        if @boxType == "Starter"
+          @weight += (value * 17.6)
+        else
+          @weight += (value * 8.6)
+        end
       end
 
       def calcFillDate(contractDate)
-
+        print "Schedule: "
+        if @boxType == "Starter"
+          print contractDate
+        else
+          # refill dates happen every 90 days from the contract start date
+          print (contractDate + 90).to_s + ", " + (contractDate + 180).to_s + ", " + (contractDate + 270).to_s + ", " + (contractDate + 360).to_s
+        end
       end
 
       def output(contractDate)
+        # outputs the box contents, priorty, and
+        # schedule. Makes calls to caclWeight,
+        # caclFilldate, and calcShipping to get
+        # correct values
 
+        if @boxType == "Starter"
+          puts "Starter Box"
+          @boxContents.each do |key, value|  
+            puts "#{value} #{key} brushes"
+            puts "#{value} #{key} replacement heads"
+            calcWeight(value)
+          end
+        else
+          puts "Refill Box"
+          @boxContents.each do |key, value|  
+            puts "#{value} #{key} replacement heads"
+            calcWeight(value)
+          end
+        end
+        puts @brushCount.to_s + " paste kits"
+        puts calcFillDate(contractDate)
+        puts calcShipping()
+        puts "\n"
       end
     end
   end
