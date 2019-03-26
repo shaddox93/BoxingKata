1f3f85ae51b9f4af7505071b5b1b0c7d083d6004
shaddox93 <haddoxshannon93@gmail.com>
2019-03-26 10:29:31 -0400

    add code to output initial preferences and create family/boxwindow classes

--- lib/boxing/kata/boxing_kata.rb
+++ lib/boxing/kata/boxing_kata.rb
@@ -2,8 +2,8 @@ require "boxing/kata/version"
 require "boxing/kata/family"
 require "boxing/kata/box"
 require "boxing/kata/graphics"
-require 'csv'
 require 'gosu'
+require 'csv'
 require 'date'
 
 module Boxing
@@ -18,7 +18,10 @@ module Boxing
       table = CSV.parse(STDIN, headers: true)
       brushes = Hash.new
       date = Date.new
-
+      
+      # reading through the table and getting the important dat that we need
+      # we need to keep track of the color and number of brushes
+      # and our contract start date
       table.each do |row|
         if row['contract_effective_date']
           date = Date.parse(row['contract_effective_date'])
@@ -30,6 +33,20 @@ module Boxing
           brushes["#{row['brush_color']}"] = 1
         end
       end
+
+      # outputing the initial brush color/count
+      if !brushes.empty?
+        puts "BRUSH PREFERENCES"
+        brushes.each do |key, value|  
+          puts "#{key}: #{value}"
+        end
+        puts "\n\n"
+      end
+
+      famData = Family.new(brushes, date) # creating a new family class
+      window = BoxWindow.new(famData) # creating a new boxwindow class
+      window.show
+      
     end
 
     def self.has_input_file?
